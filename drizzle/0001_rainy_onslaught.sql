96ecf261d1b3a6a48cba957a68f9bbd40978761a
Chris Tian <chris.tian@biposervice.com>
2026-05-03 10:59:29 +0800

    fix: add follow-up migration for writing progress fields

---
+++ drizzle/0001_rainy_onslaught.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `writing_progress` ADD `current_stage` integer;--> statement-breakpoint
+ALTER TABLE `writing_progress` ADD `current_arc` integer;--> statement-breakpoint
+ALTER TABLE `writing_progress` ADD `active_task_type` text;
